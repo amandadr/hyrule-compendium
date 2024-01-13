70bf132859f0917273105edc4fd5709bf3008208
amandadr <amandadroy@gmail.com>
2024-01-13 17:06:03 -0400

    stylize the data some more

--- src/components/EntryDetails.jsx
+++ src/components/EntryDetails.jsx
@@ -1,11 +1,45 @@
 import { useContext } from "react";
+import { makeStyles } from "@mui/styles";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { ModalContext } from "../App";
+import { common } from "@mui/material/colors";
+
+const useStyles = makeStyles(() => ({
+  modal: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    overflow: "visible",
+    maxHeight: "85vh",
+  },
+  col: {
+    width: "100%",
+    display: "flex",
+    flexFlow: "row wrap",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  box: {
+    flex: "1",
+    textAlign: "left",
+    backgroundColor: "white",
+    borderRadius: "5px",
+    border: "1px solid black",
+    overflow: "auto",
+    padding: "5px",
+    maxWidth: "70%",
+    textTransform: "capitalize",
+  },
+}));
 
 const EntryDetails = ({ data }) => {
   const { open, handleClose } = useContext(ModalContext);
+
+  const classes = useStyles();
 
   let {
     category,
@@ -27,31 +61,76 @@ const EntryDetails = ({ data }) => {
     properties = Object.entries(properties);
   }
 
-  // turn common_locations into a string
-  if (common_locations) {
-    common_locations = common_locations.join(", ");
-  }
-
-  // capitalize name, category, cooking_effect, dlc, drops, edible
-  const capitalize = (str) => typeof str === "string" && str.replace(/\b\w/g, (char) => char.toUpperCase());
-
-  name = capitalize(name);
-  category = capitalize(category);
-  cooking_effect = capitalize(cooking_effect);
-  drops = capitalize(drops);
-
   const renderTypography = (label, value) => {
-    if (value) {
+    let width = label.length * 12;
+
+    if (Array.isArray(value) && value[0]) {
       return (
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          {label}: {value}
+        <Typography
+          id="modal-modal-description"
+          sx={{ mt: 2, marginRight: 2, width: `${width}px`, flex: "40%" }}
+          className={classes.box}
+        >
+          {label}:
+          <br />
+          {Array.isArray(value[0])
+            ? value.map(([itemLabel, itemValue]) => (
+                <>
+                  {itemLabel}: {itemValue}
+                  <br />
+                </>
+              ))
+            : value.length > 5
+            ? value.join(", ")
+            : value.map((item) => (
+                <>
+                  {item}
+                  <br />
+                </>
+              ))}
+        </Typography>
+      );
+    } else if (value === true) {
+      return (
+        <Typography
+          id="modal-modal-description"
+          sx={{ mt: 2, marginRight: 2, width: `${width}px`, flex: "40%" }}
+          className={classes.box}
+        >
+          {label}: <br />
+          {value ? "Yes" : "No"}
+        </Typography>
+      );
+    } else if (value) {
+      return (
+        <Typography
+          id="modal-modal-description"
+          sx={{ mt: 2, marginRight: 2, width: `${width}px`, flex: "25%" }}
+          className={classes.box}
+        >
+          {label}:
+          {value === id ? (
+            ` ${value}`
+          ) : value.length === 0 ? (
+            <>
+              <br />
+              None
+            </>
+          ) : (
+            value && (
+              <>
+                <br />
+                {typeof value === "string" ? value : value}
+              </>
+            )
+          )}
         </Typography>
       );
     }
+
     return null;
   };
 
-  // Manipulate the data: capitalize, add spaces, etc.
   // Style the modal using MUI
 
   return (
@@ -73,30 +152,40 @@ const EntryDetails = ({ data }) => {
           boxShadow: 24,
           p: 4,
         }}
+        className={classes.modal}
       >
-        <img src={image} alt={name} style={{ width: "100px" }} />
-        <Typography id="modal-modal-title" variant="h6" component="h2">
+        <Typography
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+          mb={2}
+          sx={{
+            color: common.black,
+            fontWeight: "bold",
+            textTransform: "capitalize",
+          }}
+        >
           {name}
         </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        <img src={image} alt={name} style={{ width: "150px" }} />
+        <Typography
+          id="modal-modal-description"
+          sx={{ mt: 2, fontSize: "12px" }}
+        >
           {description}
         </Typography>
-        {renderTypography("Category", category)}
-        {renderTypography("Common Locations", common_locations)}
-        {renderTypography("Cooking Effect", cooking_effect)}
-        {renderTypography("DLC", dlc)}
-        {renderTypography("Drops", drops)}
-        {renderTypography("Edible", edible)}
-        {renderTypography("Hearts Recovered", hearts_recovered)}
-        {renderTypography("ID", id)}
-        {properties &&
-          properties.map((property) => {
-            return (
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                {capitalize(property[0])}: {property[1]}
-              </Typography>
-            );
-          })}
+        <br />
+        <Box className={classes.col}>
+          {renderTypography("Common Locations", common_locations)}
+          {renderTypography("ID", id)}
+          {renderTypography("DLC", dlc)}
+          {/* {renderTypography("Category", category)} */}
+          {renderTypography("Edible", edible)}
+          {renderTypography("Hearts", hearts_recovered)}
+          {renderTypography("Cooking Effect", cooking_effect)}
+          {renderTypography("Drops", drops)}
+          {renderTypography("Stats", properties)}
+        </Box>
       </Box>
     </Modal>
   );
