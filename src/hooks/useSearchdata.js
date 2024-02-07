ed0221d9c6f25506cbc868e0bfc12954e5f8d326
amandadr <amandadroy@gmail.com>
2024-02-07 15:09:05 -0400

    implement useSearchData hook

---
+++ src/hooks/useSearchdata.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const searchEndpoint = (search) =>
+  `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${search}`;
+
+function useSearchData(initialSearch = "") {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async (search) => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const endpoint = searchEndpoint(search);
+        const response = await axios.get(endpoint);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData(initialSearch);
+  }, [initialSearch]);
+
+  return { data, isLoading, error };
+}
+
+export default useSearchData;
