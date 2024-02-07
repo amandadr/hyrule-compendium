946d35fa5ca78e12256dac6d46cc5c1a555e81b1
amandadr <amandadroy@gmail.com>
2024-02-07 18:18:55 -0400

    hit enter to search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,6 +9,16 @@ function SearchBar() {
 
   const handleSearchInputChange = (event) => {
     setSearchQuery(event.target.value);
+    // Execute a function when the user presses a key on the keyboard
+    const input = document.getElementById("search-input");
+    input.addEventListener("keypress", function (event) {
+      // If the user presses the "Enter" key on the keyboard
+      if (event.key === "Enter") {
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("search-button").click();
+      }
+    });
   };
 
   const handleSearchClick = () => {
@@ -21,23 +31,32 @@ function SearchBar() {
   return (
     <div className="search-container">
       <input
+        id="search-input"
         type="text"
         value={searchQuery}
         onChange={handleSearchInputChange}
-        placeholder="Search..."
+        placeholder="Search by name or ID..."
         className="search-input"
       />
-      <button onClick={handleSearchClick} className="search-button">
+      <button
+        onClick={handleSearchClick}
+        className="search-button"
+        id="search-button"
+      >
         Search
       </button>
-
       <div className="search-results">
         {isLoading && <div>Loading...</div>}
 
         {error && !isLoading && <div>We couldn't find that in Hyrule!</div>}
 
         {data && !error && !isLoading && (
-          <ListEntry data={data} className="search-item" key={data.id} />
+          <ListEntry
+            data={data}
+            className="search-item"
+            subinfo={data.id}
+            key={data.id}
+          />
         )}
       </div>
     </div>
