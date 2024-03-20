50fbbb0d4c1de213f5de91b2100e1de8b9b06ded
amandadr <amandadroy@gmail.com>
2024-03-20 12:26:41 -0300

    integrate Python fuzzy search (Lambda func); removed onChange listener

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useSearchData from "../hooks/useSearchData";
 import ListEntry from "./ListEntry";
 import "../styles/Search.scss";
@@ -7,25 +7,18 @@ function SearchBar() {
   const [searchQuery, setSearchQuery] = useState("");
   const { data, isLoading, error, fetchData } = useSearchData();
 
-  const handleSearchInputChange = (event) => {
-    setSearchQuery(event.target.value);
-    // Execute a function when the user presses a key on the keyboard
-    const input = document.getElementById("search-input");
+  const input = document.getElementById("search-input");
+  const handleSearchInputChange = (inputEvent) => {
     input.addEventListener("keypress", function (event) {
       // If the user presses the "Enter" key on the keyboard
       if (event.key === "Enter") {
         event.preventDefault();
+        // Set the state to the value of the input element and
+        setSearchQuery(inputEvent.target.value);
         // Trigger the button element with a click
         document.getElementById("search-button").click();
       }
     });
-  };
-
-  const handleSearchClick = () => {
-    // Only fetch if there's a non-empty query
-    if (searchQuery.trim() !== "") {
-      fetchData(searchQuery);
-    }
   };
 
   return (
@@ -34,32 +27,43 @@ function SearchBar() {
         <input
           id="search-input"
           type="text"
-          value={searchQuery}
           onChange={handleSearchInputChange}
           placeholder="Search by name or ID..."
           className="search-input"
         />
         <button
-          onClick={handleSearchClick}
+          onClick={useEffect(() => {
+            if (searchQuery.trim() !== "") {
+              fetchData(searchQuery);
+            }
+          }, [searchQuery])}
           className="search-button"
           id="search-button"
         >
           Search
         </button>
       </div>
-      <div className="search-results">
+      <div className="search-results-container">
         {isLoading && <div>Loading...</div>}
 
         {error && !isLoading && <div>We couldn't find that in Hyrule!</div>}
 
-        {data && !error && !isLoading && (
-          <ListEntry
-            data={data}
-            className="search-item"
-            subinfo={data.id}
-            key={data.id}
-          />
+        {data.length > 0 && !error && !isLoading && (
+          <div className="search-results">
+            {data.map((data) => (
+              <ListEntry
+                data={data.data}
+                className="search-item"
+                subinfo={data.data.id}
+                key={data.data.id}
+              />
+            ))}
+          </div>
         )}
+        {data.length > 0 &&
+          !error &&
+          !isLoading &&
+          `${data.length} results found.`}
       </div>
     </div>
   );
--- src/hooks/useSearchData.js
+++ src/hooks/useSearchData.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
 
+const fuzzyEndpoint = `https://bpkb9up1mj.execute-api.us-east-1.amazonaws.com/fuzzy-search`; // Lambda function hosting Python code
+
 const searchEndpoint = (search) =>
   `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/` + search;
 
+const fuzzy = async (search) =>
+  await axios.get(fuzzyEndpoint, {
+    params: { search_term: search },
+  });
+
 function useSearchData() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]); // data = array of objects
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -14,9 +21,18 @@ function useSearchData() {
     setError(null);
 
     try {
-      const endpoint = searchEndpoint(search);
-      const response = await axios.get(endpoint);
-      setData(response.data.data);
+      const fuzzyTerms = await fuzzy(search);
+      let searchResults = [];
+      const apiEndpoints = fuzzyTerms.data.map((term) => searchEndpoint(term));
+      const endpointsArray = apiEndpoints.map((endpoint) =>
+        axios.get(endpoint)
+      );
+      const apiResponses = await Promise.all(endpointsArray);
+      searchResults = apiResponses.map((response) => response.data);
+
+      searchResults.length > 0
+        ? setData(searchResults)
+        : setError("No results found");
     } catch (error) {
       setError(error);
     } finally {
